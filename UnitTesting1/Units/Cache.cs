03783a175776f6fdc4ba2a50683f198430048507
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2026-01-19 12:45:33 -0300

    Create Cache class

---
+++ UnitTesting1/Units/Cache.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTesting1.Units
+{
+    public class Cache
+    {
+        public record Item(string Url, string Content, DateTime LastCollected);
+
+        private readonly TimeSpan _cacheTime;
+        private readonly Dictionary<string, Item> _cache = new Dictionary<string, Item>();
+
+        public Cache(TimeSpan cacheTime)
+        {
+            _cacheTime = cacheTime;
+        }
+
+        public bool Contains(string url)
+        {
+            if(_cache.TryGetValue(url, out Item? item))
+            {
+                return DateTime.UtcNow.Subtract(item.LastCollected) > _cacheTime;
+            }
+
+            return false;
+        }
+
+        public void Add(Item item) => _cache.Add(item.Url, item);
+    }
+}
