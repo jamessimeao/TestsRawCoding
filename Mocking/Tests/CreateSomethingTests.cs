89c95137959414dd1d7afbae3b0640f1137e5a7e
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2026-01-19 15:32:48 -0300

    Create mock for IStore

---
+++ Mocking/Tests/CreateSomethingTests.cs
@@ -0,0 +1,20 @@
+﻿using Mocking.Units;
+
+namespace Mocking.Tests
+{
+    public class CreateSomethingTests
+    {
+        public class StoreMock : CreateSomething.IStore
+        {
+            public int SaveAttempts { get; set; }
+
+            public bool Save(CreateSomething.Something something)
+            {
+                SaveAttempts++;
+                return false;
+            }
+        }
+
+
+    }
+}
