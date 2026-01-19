8ed2f6f23370d8bd487c45ac44d4ded052edcab9
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2026-01-19 11:51:26 -0300

    Create test for saving item in TodoList

---
+++ UnitTesting1/Tests/TodoListTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnitTesting1.Units;
+using static UnitTesting1.Units.TodoList;
+
+namespace UnitTesting1.Tests
+{
+    public class TodoListTests
+    {
+        [Fact]
+        public void Add_SavesTodoItem()
+        {
+            // arrange
+            TodoList list = new TodoList();
+
+            // act
+            const string content = "Test Content";
+            list.Add(new TodoItem(content));
+
+            // assert
+            TodoItem savedItem = Assert.Single(list.All);
+            Assert.NotNull(savedItem);
+            Assert.Equal(content, savedItem.Content);
+        }
+    }
+}
