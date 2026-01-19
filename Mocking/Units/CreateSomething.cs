ef896c96e7805b5ff591028348f67e97d3e71f75
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2026-01-19 15:28:53 -0300

    Create CreateSomething class

---
+++ Mocking/Units/CreateSomething.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mocking.Units
+{
+    public class CreateSomething
+    {
+        public class Something
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+
+        public interface IStore
+        {
+            bool Save(Something something);
+        }
+
+        private readonly IStore _store;
+
+        public CreateSomething(IStore store)
+        {
+            _store = store;
+        }
+
+        public CreateSomethingResult Create(Something something)
+        {
+            if (something is { Name: { Length: > 0 } })
+            {
+                return new(_store.Save(something));
+            }
+
+            return new(false, "Somethings not valid.");
+        }
+
+        public record CreateSomethingResult(bool Success, string Error = "");
+    }
+}
