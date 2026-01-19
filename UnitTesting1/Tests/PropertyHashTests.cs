8bfeac539bc4da192ac4b80a888430d6de7a1203
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2026-01-19 15:05:19 -0300

    Create test AlgorithmPropertyHash_AppliesHashingAlgorithmToSeed

--- UnitTesting1/Tests/PropertyHashTests.cs
+++ UnitTesting1/Tests/PropertyHashTests.cs
@@ -1,4 +1,5 @@
-﻿using UnitTesting1.Units;
+﻿using System.Data;
+using UnitTesting1.Units;
 
 namespace UnitTesting1.Tests
 {
@@ -13,6 +14,15 @@ namespace UnitTesting1.Tests
             string hash = hasher.Hash(item, itm => itm.Url, itm => itm.Content);
 
             Assert.Equal("urlcontent", hash);
+        }
+
+        [Fact]
+        public void AlgorithmPropertyHash_AppliesHashingAlgorithmToSeed()
+        {
+            var hasher = new AlgorithmPropertyHash("sha256");
+            Cache.Item item = new Cache.Item("url", "content", DateTime.Now);
+            string hash = hasher.Hash(item, itm => itm.Url, itm => itm.Content);
+            Assert.Equal("9FyLxk+9z73XO8xhZ15emMaK+oa8aDg6LWiY6y40KyQ=", hash);
         }
     }
 }
