1d63de8be91184fa47d2d24336960969d3644241
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2026-01-19 13:04:47 -0300

    Create test CachesTimeWithinTimeSpan

---
+++ UnitTesting1/Tests/CacheTests.cs
@@ -0,0 +1,19 @@
+﻿using UnitTesting1.Units;
+
+namespace UnitTesting1.Tests
+{
+    public class CacheTests
+    {
+        [Fact]
+        public void CachesItemWithinTimeSpan()
+        {
+            Cache cache = new Cache(TimeSpan.FromDays(1));
+            const string url = "url";
+            cache.Add(new(url, "content", DateTime.Now));
+
+            bool contains = cache.Contains(url);
+
+            Assert.True(contains);
+        }
+    }
+}
