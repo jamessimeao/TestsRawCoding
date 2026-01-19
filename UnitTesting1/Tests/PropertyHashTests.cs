057904d628b317c2d85b9a21d31da95473e54651
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2026-01-19 14:50:18 -0300

    Create test PropertyHash_ConcatenateSelectedFieldInOrder

---
+++ UnitTesting1/Tests/PropertyHashTests.cs
@@ -0,0 +1,18 @@
+﻿using UnitTesting1.Units;
+
+namespace UnitTesting1.Tests
+{
+    public class PropertyHashTests
+    {
+        [Fact]
+        public void PropertyHash_ConcatenateSelectedFieldInOrder()
+        {
+            PropertyHash hasher = new PropertyHash();
+            Cache.Item item = new Cache.Item("url", "content", DateTime.Now);
+
+            string hash = hasher.Hash(item, itm => itm.Url, itm => itm.Content);
+
+            Assert.Equal("urlcontent", hash);
+        }
+    }
+}
